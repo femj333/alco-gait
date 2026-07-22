7c16fbbe0c0932812725041d2a9660ea3fbcdfbf
femj333 <fjansen1715@gmail.com>
2026-07-22 10:44:42 -0400

    background layer

--- app/src/main/java/wpics/alcogait/ui/HomeScreen.kt
+++ app/src/main/java/wpics/alcogait/ui/HomeScreen.kt
@@ -4,23 +4,28 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat.getDrawable
 import com.google.accompanist.drawablepainter.rememberDrawablePainter
 import wpics.alcogait.R
 import wpics.alcogait.ui.components.TopBar
 import wpics.alcogait.ui.theme.DarkBlue
+import wpics.alcogait.ui.theme.LightBlue
 
 @Composable
 fun HomeScreen() {
@@ -31,30 +36,43 @@ fun HomeScreen() {
         topBar = {
             TopBar()
         }
-    ) {
-        Column(
+    ) { padding ->
+
+        // background layer
+        Background(padding)
+
+        //
+        Box(
+            modifier = Modifier
+                .size(width = 200.dp, height = 100.dp)
+                .background(color = LightBlue)
+        )
+    }
+}
+
+@Composable
+fun Background(padding: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
+    ){
+        Image(
+            modifier = Modifier,
+            painter = rememberDrawablePainter(
+                drawable = getDrawable(
+                    LocalContext.current,
+                    R.drawable.sober
+                )
+            ),
+            contentDescription = "Sober character animation",
+            contentScale = ContentScale.FillWidth,
+        )
+
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it)
-        ){
-            Image(
-                modifier = Modifier,
-                painter = rememberDrawablePainter(
-                    drawable = getDrawable(
-                        LocalContext.current,
-                        R.drawable.sober
-                    )
-                ),
-                contentDescription = "Sober character animation",
-                contentScale = ContentScale.FillWidth,
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = DarkBlue)
-            )
-
-        }
+                .background(color = DarkBlue)
+        )
     }
 }
