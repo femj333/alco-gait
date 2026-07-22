cf2b7cf473466b7e927856bd5a2d0815a556ebfc
femj333 <fjansen1715@gmail.com>
2026-07-22 12:19:57 -0400

    drunk state sign

--- app/src/main/java/wpics/alcogait/ui/HomeScreen.kt
+++ app/src/main/java/wpics/alcogait/ui/HomeScreen.kt
@@ -2,30 +2,45 @@ package wpics.alcogait.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.dropShadow
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.shadow.Shadow
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat.getDrawable
 import com.google.accompanist.drawablepainter.rememberDrawablePainter
 import wpics.alcogait.R
 import wpics.alcogait.ui.components.TopBar
 import wpics.alcogait.ui.theme.DarkBlue
 import wpics.alcogait.ui.theme.LightBlue
+import wpics.alcogait.ui.theme.LightPink
 
 @Composable
 fun HomeScreen() {
@@ -38,24 +53,126 @@ fun HomeScreen() {
         }
     ) { padding ->
 
-        // background layer
-        Background(padding)
-
-        //
         Box(
             modifier = Modifier
-                .size(width = 200.dp, height = 100.dp)
-                .background(color = LightBlue)
-        )
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            // background layer
+            Background()
+
+            // drunk state sign
+            /* TODO -> change state based on readings */
+            Box(
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .offset(y = 35.dp)
+            ) {
+                DrunkStateSign()
+            }
+
+            // uber button
+
+            // lyft button
+
+            // start button
+
+        }
     }
 }
 
 @Composable
-fun Background(padding: PaddingValues) {
+fun DrunkStateSign(){
+    Box(
+        modifier = Modifier
+            .size(width = 275.dp, height = 110.dp)
+            .dropShadow(
+                shape = RoundedCornerShape(10.dp),
+                shadow = Shadow(
+                    radius = 1.dp,
+                    spread = 2.dp,
+                    color = Color(0x40000000),
+                    offset = DpOffset(x = (-2).dp, 8.dp)
+                )
+            )
+            .clip(RoundedCornerShape(10.dp))
+            .background(color = LightBlue)
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .offset(x = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(11.dp),
+
+            ){
+            repeat(6) {
+                Circle()
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .offset(x = 260.dp),
+            verticalArrangement = Arrangement.spacedBy(11.dp),
+
+            ){
+            repeat(6) {
+                Circle()
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .offset(y = 9.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+
+            ){
+            repeat(15) {
+                Circle()
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .offset(y = (-9).dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+
+            ){
+            repeat(15) {
+                Circle()
+            }
+        }
+
+        Text(
+            text = "SOBER",
+            color = LightPink,
+            fontSize = 65.sp,
+            modifier = Modifier
+                .align(Alignment.Center)
+        )
+    }
+}
+
+
+@Composable
+fun Circle() {
+    Box(
+        modifier = Modifier
+            .size(6.dp)
+            .clip(CircleShape)
+            .background(LightPink)
+    )
+}
+
+
+@Composable
+fun Background() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(padding)
     ){
         Image(
             modifier = Modifier,
--- app/src/main/java/wpics/alcogait/ui/theme/Type.kt
+++ app/src/main/java/wpics/alcogait/ui/theme/Type.kt
@@ -1,12 +1,14 @@
 package wpics.alcogait.ui.theme
 
 import androidx.compose.material3.Typography
+
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import wpics.alcogait.R
+
 
 // Set of Material typography styles to start with
 
